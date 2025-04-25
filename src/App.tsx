ef3f3571aa60c647282180fcc0150dae04b3d357
kyy-95631488 <riskir5622@gmail.com>
2025-04-25 14:39:34 +0700

    Update #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import LL1 from "./pages/ll1";
